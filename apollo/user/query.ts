07520ca3016521d5f73dfe6a45f49f1f872e0bb6
Azi-png <azizagoibnazarova24@gmail.com>
2025-07-14 14:09:25 +0900

    fix: modify GraphQL query command

--- apollo/user/query.ts
+++ apollo/user/query.ts
@@ -43,10 +43,10 @@ export const GET_AGENTS = gql`
 `;
 
 export const GET_MEMBER = gql(`
-query GetMember($input: String!) {
-    getMember(memberId: $input) {
-        _id
-        memberType
+   query GetMember($input: String!) {
+          getMember(memberId: $input) {
+             _id
+          memberType
         memberStatus
         memberAuthType
         memberPhone
